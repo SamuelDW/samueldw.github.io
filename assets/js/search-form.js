eab01a3ec65b592cbf7a663dd8c4dc9d9eb22d49
Samuel DW <s.durrantwalker@yordasgroup.com>
2022-03-31 15:44:36 +0100

    Search bar now works correctly displaying on all devices

--- assets/js/search-form.js
+++ assets/js/search-form.js
@@ -4,6 +4,7 @@ const searchButton = document.querySelector('#search-button'),
 
 searchButton.addEventListener('click', function(e) {
     e.preventDefault()
+    extraFields.classList.add('hidden')
 })
 
 
