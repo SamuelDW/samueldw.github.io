e4a70fd95c342b95f22b69edd343cca65358830d
Samuel DW <s.durrantwalker@yordasgroup.com>
2022-03-31 10:07:01 +0100

    Changed form to be mobile first, shows extra fields on filter click

--- assets/js/search-form.js
+++ assets/js/search-form.js
@@ -1,5 +1,12 @@
-const searchButton = document.querySelector('#search-button')
+const searchButton = document.querySelector('#search-button'),
+    filterButton = document.querySelector('#filter-button'),
+    extraFields =  document.querySelector('#extra-fields')
 
 searchButton.addEventListener('click', function(e) {
     e.preventDefault()
+})
+
+
+filterButton.addEventListener('click', function (e) {
+    extraFields.classList.toggle('hidden')
 })
